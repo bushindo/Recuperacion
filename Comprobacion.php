cd497a93c613ab8fdf7538d7ddec5543f58febe4
a022590417r <a022590417r@S04-PC02>
2016-04-20 17:06:41 +0200

    falta de comprobaciones

--- Comprobacion.php
+++ Comprobacion.php
@@ -5,7 +5,7 @@
  * and open the template in the editor.
  */
 
-$nombre= $_POST['nombre'];
+$nombre= $_REQUEST['nombre'];
 $fechana= $_REQUEST['fechana'];
 $dni= $_REQUEST['dni'];
 $apellidos= $_REQUEST['apellidos'];
@@ -17,14 +17,96 @@ $telefono= $_REQUEST['telefono'];
 $ocupacion= $_REQUEST['ocupacion'];
 $comentarios= $_REQUEST['comentarios'];
 
-echo "Nombre: ". $nombre ."<br/>";
-echo "Apellidos: ". $apellidos ."<br/>";
-echo "FechaNacimiento: " . $fechana ."<br/>";
-echo "DNI: ". $dni ."<br/>";
-echo "Direccion: ". $direccion ."<br/>";
-echo "CodigoPostal: " . $CP ."<br/>";
-echo "Ciudad: ". $cuidad ."<br/>";
-echo"Provincia: ". $provincia ."<br/>";
-echo"Telefono: ". $telefono ."<br/>";
-echo "Ocupacion: ".$ocupacion ."<br/>";
-echo "Comentarios: ". $comentarios ."<br/>";
+if ($_POST) {
+    if ($nombre==""){
+	echo "No ha introducido ningun nombre."."<br/>";
+    }else {
+        echo "Nombre: ". $nombre ."<br/>";
+    }
+}
+
+if ($_POST) {
+    if ($apellidos==""){
+        echo "No ha introducido ningun apellido."."<br/>";
+    } else {
+        echo "Apellidos: ". $apellidos ."<br/>";
+    }
+}
+
+if ($_POST) {
+    if ($fechana==""){
+	echo "No ha introducido ninguna fecha de nacimiento."."<br/>";
+    }else {
+        echo "Fecha Nacimiento: ". $fechana ."<br/>";
+    }
+}
+
+if ($_POST) {
+    if ($dni==""){
+	echo "No ha introducido ningun DNI."."<br/>";
+    }else {            
+        echo "DNI: ". $dni ."<br/>";
+    }
+}
+
+if ($_POST) {
+    if ($direccion==""){
+	echo "No ha introducido ninguna direccion."."<br/>";
+    }else {
+        echo "Direccion: ". $direccion ."<br/>";
+    }
+}
+
+if ($_POST) {
+    if ($CP==""){
+	echo "No ha introducido ningun codigo postal."."<br/>";
+    }else {
+        echo "Codigo Postal: ". $CP ."<br/>";
+    }
+}
+
+if ($_POST) {
+    if ($cuidad==""){
+        echo "No ha introducido ninguna ciudad."."<br/>";
+    }else {
+        echo "Ciudad: ". $ciudad ."<br/>";
+    }
+}
+
+if ($_POST) {
+    if ($provincia==""){
+	echo "No ha introducido ninguna provincia."."<br/>";
+    }else {            
+        echo "Provincia: ". $provincia ."<br/>";
+    }
+}
+
+if ($_POST) {
+    if ($telefono==""){
+	echo "No ha introducido ningun telefono."."<br/>";
+    }else {            
+        echo "Telefono: ". $telefono ."<br/>";
+    }
+}
+
+if ($_POST) {
+    if ($ocupacion==""){
+	echo "No ha introducido ninguna ocupacion."."<br/>";
+    }else {            
+        echo "Ocupacion: ". $ocupacion ."<br/>";
+    }
+}
+
+if ($_POST) {
+    if ($comentarios==""){
+	echo "No ha introducido ningun comentario."."<br/>";
+    }else {            
+        echo "Comentarios: ". $comentarios ."<br/>";
+    }
+}
+
+function letra_nif($dni) {
+        return substr("TRWAGMYFPDXBNJZSQVHLCKE",strtr($dni,"XYZ","012")%23,1);
+    }
+    $numero = $dni;
+    echo 'El NIF del DNI "'.$numero.'" es "'.$numero.letra_nif($numero).'"<br/>';
