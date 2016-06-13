f0c72d0da51087bc3d67264750d494c90d835fe8
bushindo <alromo7@gmail.com>
2016-06-13 14:55:37 +0200

    Acabado

--- formulario.php
+++ formulario.php
@@ -1,60 +1,5 @@
 <html>
 
-<FORM method="post" action="formulario.php">
-<title>Formulario</title>
-<CENTER>
-<TABLE border="0">
-    <h1>Datos personales<br></h1>
-    
-<TR>
-   <TD>Nombre:</TD>
-   <TD> <INPUT TYPE="text"  SIZE=18 MAXLENGTH=18 name="nombre"> 
-       F.Nac.:<INPUT TYPE="text"  SIZE=10 MAXLENGTH=10 name="fechana"> 
-       DNI:   <INPUT TYPE="text"  SIZE=12 name="dni"></TD>
-
-<TR>
-   <TD>Apellidos:</TD>
-   <TD> <INPUT TYPE="text"  SIZE=48 MAXLENGTH=48 name="apellidos"></TD>
-
-<TR>
-   <TD>Calle y numero:</TD>
-   <TD> <INPUT TYPE="text"  SIZE=48 MAXLENGTH=48 name="direccion"></TD>
-
-<TR>
-  <TD>Codigo Postal:</TD>
-  <TD> <INPUT TYPE="text"  SIZE=5 MAXLENGTH=5 name="CP">
-      Ciudad:  <INPUT TYPE="text"  SIZE=36 MAXLENGTH=36 name="ciudad"></TD>
-
-<TR>
-   <TD>Provincia: </TD> 
-   <TD> <INPUT TYPE="text"  SIZE=20 MAXLENGTH=20 name="provincia">
-       Telefono: <INPUT TYPE="text"  SIZE=9 MAXLENGTH=9 name="telefono"></TD>
-
-<TR>
-   <TD>Ocupacion:</TD>
-   <TD> <SELECT name="ocupacion">
-                <OPTION VALUE=""></OPTION>
-		<OPTION VALUE="estudiante">Estudiante</OPTION>
-		<OPTION VALUE="empleado">Trabajador por cuenta ajena</OPTION>
-		<OPTION VALUE="autonomo">Autonomo</OPTION>
-		<OPTION VALUE="jubilado">Jubilado</OPTION>
-		<OPTION VALUE="desempleado">Desempleado</OPTION>
-		<OPTION VALUE="otro">Otro</OPTION>
-	</SELECT>Escoja una opcion</TD>
-
-<TR>
-   <TD>Comentarios<BR> personales:</TD>
-   <TD> <TEXTAREA rows="5" cols="48" name="comentarios" ></TEXTAREA></TD>
-
-<TR>
-   <TD><B>Pulse aqui:</B></TD>
-   <TD ALIGN=CENTER>
-       <INPUT TYPE="submit" VALUE="Enviar datos ">
-       <INPUT TYPE="reset" VALUE="Borrar los datos"></TD>
-
-</TABLE>
-</CENTER>
-</FORM>
 <?php
 /* 
  * To change this license header, choose License Headers in Project Properties.
@@ -66,6 +11,21 @@ function letra_nif($dni) {
         return substr("TRWAGMYFPDXBNJZSQVHLCKE",strtr($dni,"XYZ","012")%23,1);
 }
 
+$nombre= "";
+$fechana= "";
+$dni= "";
+$apellidos= "";
+$direccion= "";
+$CP= "";
+$ciudad= "";
+$provincia= "";
+$telefono= "";
+$ocupacion= "";
+$comentarios= "";
+$mostrarForm=1;
+
+if ($_POST) {
+    $mostrarForm=0;
     $nombre= $_REQUEST['nombre'];
     $fechana= $_REQUEST['fechana'];
     $dni= $_REQUEST['dni'];
@@ -77,111 +37,134 @@ function letra_nif($dni) {
     $telefono= $_REQUEST['telefono'];
     $ocupacion= $_REQUEST['ocupacion'];
     $comentarios= $_REQUEST['comentarios'];
-    
-if ($_POST) {
- if ($nombre==""){
-	echo "No ha introducido ningun nombre."."<br/>";
-    }else {
-        echo "Nombre: ". $nombre ."<br/>";
-    }
-}
-
-if ($_POST) {
+    if ($nombre==""){
+        echo "No ha introducido ningun nombre."."<br/>";
+        $mostrarForm=1;
+    } else {
+        echo "Nombre: " .$nombre. "<br/>";
+    }  
     if ($apellidos==""){
         echo "No ha introducido ningun apellido."."<br/>";
-    } else {
-        echo "Apellidos: ". $apellidos ."<br/>";
+         $mostrarForm=1;
+    }else {
+        echo "Apellidos: " .$apellidos. "<br/>";
     }
-}
-
-if ($_POST) {
     if ($fechana==""){
-	echo "No ha introducido ninguna fecha de nacimiento."."<br/>";
+        echo "No ha introducido ninguna fecha de nacimiento."."<br/>";
+         $mostrarForm=1;
     }else {
-        echo "Fecha Nacimiento: ". $fechana ."<br/>";
+        echo "F.Nacimiento: " .$fechana. "<br/>";
     }
-}
-
-if ($_POST) {
     if ($dni==""){
-	echo "No ha introducido ningun DNI."."<br/>";
-    }else {            
-        if (strlen($dni) != 8){
-            echo "El DNI tiene que tener 8 digitos"."<br/>";
-        }else {
-            echo "DNI: ". $dni ."<br/>";
-        }
-    }
-}
-
-if ($_POST) {
+        echo "No ha introducido ningun DNI."."<br/>";
+         $mostrarForm=1;
+     }else {
+         echo "DNI: " .$dni. " ";
+         $numero = $dni;
+    echo '  La letra del DNI "'.$numero.'" es "'.letra_nif($numero).'"<br/>';
+     }
+    
     if ($direccion==""){
-	echo "No ha introducido ninguna direccion."."<br/>";
+        echo "No ha introducido ninguna direccion."."<br/>";
+         $mostrarForm=1;
     }else {
-        echo "Direccion: ". $direccion ."<br/>";
+        echo "Direccion: " .$direccion. "<br/>";
     }
-}
-
-if ($_POST) {
     if ($CP==""){
-	echo "No ha introducido ningun codigo postal."."<br/>";
+        echo "No ha introducido ningun codigo postal."."<br/>";
+         $mostrarForm=1;
     }else {
-        if (strlen($CP) != 5){
-            echo "El codigo postal tiene que tener 5 digitos"."<br/>";
-        }else {
-            echo "Codigo Postal: ". $CP ."<br/>";
-        }
+        echo "CodigoPostal: " .$CP. "<br/>";
     }
-}
-
-if ($_POST) {
     if ($ciudad==""){
         echo "No ha introducido ninguna ciudad."."<br/>";
+         $mostrarForm=1;
     }else {
-        echo "Ciudad: ". $ciudad ."<br/>";
+        echo "Ciudad: " .$ciudad. "<br/>";
     }
-}
-
-if ($_POST) {
     if ($provincia==""){
-	echo "No ha introducido ninguna provincia."."<br/>";
-    }else {            
-        echo "Provincia: ". $provincia ."<br/>";
+        echo "No ha introducido ninguna provincia"."<br/>";
+         $mostrarForm=1;
+    }else {
+        echo "Provincia: " .$provincia. "<br/>";
     }
-}
-
-if ($_POST) {
     if ($telefono==""){
-	echo "No ha introducido ningun telefono."."<br/>";
-    }else {            
-        if (strlen($telefono) != 9){
-            echo "El telefono tiene que tener 9 digitos"."<br/>";
-        }else {
-            echo "Telefono: ". $telefono ."<br/>";
-        }
+        echo "No ha introducido ningun numero de telefono."."<br/>";
+         $mostrarForm=1;
+    }else {
+        echo "Telefono: " .$telefono. "<br/>";
     }
-}
-
-if ($_POST) {
     if ($ocupacion==""){
-	echo "No ha introducido ninguna ocupacion."."<br/>";
-    }else {            
-        echo "Ocupacion: ". $ocupacion ."<br/>";
+        echo "No ha introducido ninguna ocupacion."."<br/>";
+         $mostrarForm=1;
+    }else {
+        echo "Ocupacion: " .$ocupacion. "<br/>";
     }
-}
-
-if ($_POST) {
     if ($comentarios==""){
-	echo "No ha introducido ningun comentario."."<br/>";
-    }else {            
-        echo "Comentarios: ". $comentarios ."<br/>";
-    }
+        echo "No ha introducido ningun comentario final."."<br/>";
+         $mostrarForm=1;
+    }else {
+        echo "Comentarios: " .$comentarios. "<br/>";
+     }
+    echo "<a href=./formulario.html> Volver al formulario</a>";
 }
-$numero = $dni;
-    echo 'El NIF del DNI "'.$numero.'" es "'.letra_nif($numero).'"<br/>';
-
-
-
-?>       
+if ( $mostrarForm != 0 ) { 
+echo <<<EOF
+      
+ 
+<FORM method="post" action="formulario.php">
+<title>Formulario</title>
+<CENTER>
+<TABLE border="0">
+    <h1>Datos personales<br></h1>
     
+<TR>
+   <TD>Nombre:</TD>
+   <TD> <INPUT TYPE="text" value='$nombre' SIZE=18 MAXLENGTH=18 name="nombre"> 
+       F.Nac.:<INPUT TYPE="text" value='$fechana'  SIZE=10 MAXLENGTH=10 name="fechana"> 
+       DNI:   <INPUT TYPE="text" value='$dni' SIZE=12 name="dni"></TD>
+
+<TR>
+   <TD>Apellidos:</TD>
+   <TD> <INPUT TYPE="text" value='$apellidos' SIZE=48 MAXLENGTH=48 name="apellidos"></TD>
+
+<TR>
+   <TD>Calle y numero:</TD>
+   <TD> <INPUT TYPE="text" value='$direccion'  SIZE=48 MAXLENGTH=48 name="direccion"></TD>
+
+<TR>
+  <TD>Codigo Postal:</TD>
+  <TD> <INPUT TYPE="text" value='$CP' SIZE=5 MAXLENGTH=5 name="CP">
+      Ciudad:  <INPUT TYPE="text" value='$ciudad' SIZE=36 MAXLENGTH=36 name="ciudad"></TD>
+
+<TR>
+   <TD>Provincia: </TD> 
+   <TD> <INPUT TYPE="text" value='$provincia' SIZE=20 MAXLENGTH=20 name="provincia">
+       Telefono: <INPUT TYPE="text" value='$telefono' SIZE=9 MAXLENGTH=9 name="telefono"></TD>
+
+<TR>
+   <TD>Ocupacion:</TD>
+   <TD> <SELECT name="ocupacion">
+                <OPTION VALUE=""></OPTION>
+               echo' <OPTION   VALUE="Estudiante">Estudiante</OPTION>';
+		<OPTION VALUE="Empleado">Trabajador por cuenta ajena</OPTION>
+		<OPTION VALUE="Autonomo">Autonomo</OPTION>
+		<OPTION VALUE="Jubilado">Jubilado</OPTION>
+		<OPTION VALUE="Desempleado">Desempleado</OPTION>
+		<OPTION VALUE="Otro">Otro</OPTION>
+	</SELECT>Escoja una opcion</TD>
+<TR>
+   <TD>Comentarios<BR> personales:</TD>
+   <TD> <TEXTAREA rows="5" cols="48" name="comentarios"></TEXTAREA></TD>
+<TR>
+   <TD><B>Pulse aqui:</B></TD>
+   <TD ALIGN=CENTER>
+       <INPUT TYPE="submit" name="enviar" VALUE="Enviar datos">
+       <INPUT TYPE="reset" VALUE="Borrar los datos"></TD>
+</TABLE>
+</CENTER>
+</FORM>
+EOF;
+}
+?>          
 </html>
